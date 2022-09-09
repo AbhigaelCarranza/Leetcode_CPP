b53cecf8b5523058fb6337c3d784c7daa4dce0ef
AbhigaelCarranza <abhicarranz@gmail.com>
2022-09-09 15:15:06 -0700

    Tarea de Udemy

---
+++ UdemyWork.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void numberGuay(int );
+
+int main()
+{
+    numberGuay(6);
+    return 0;
+}
+
+void numberGuay(int number)
+{
+    int value=0;
+    for(int i=1; i<number; i++)
+    {
+        value+=i;
+        if(value==number)
+        {
+            cout << "El numero es guay"<<endl;
+            break;
+        }
+        if(value>number)
+        {
+            cout << "El numero no es guay "<< endl;
+            break;
+        }
+    }
+    cout << "Value= "<<value<<endl;
+    cout << "Number="<<number<<endl;
+}
